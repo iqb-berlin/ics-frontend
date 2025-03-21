84a4191a241fc5bbbc8d5445179e2465b181aaa3
paflov <philipp.franck@hu-berlin.de>
2025-03-21 10:21:39 +0100

    Frontend: Only pull task when on overview tab

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -40,6 +40,7 @@ export class TaskComponent implements OnInit, OnDestroy {
   }
 
   protected tabs: TaskTab[] = [];
+  private currentTab: string = 'overview';
   private readonly subscriptions: { [key: string]: Subscription } = { };
 
   ngOnInit(): void {
@@ -58,15 +59,10 @@ export class TaskComponent implements OnInit, OnDestroy {
       ).subscribe(task => {
         this.subscriptions['polling'] = interval(1000)
           .pipe(
+            filter(i => this.currentTab === 'overview'),
             switchMap(() => this.ds.task ? this.ds.getTask(this.ds.task.id) : of(null)),
             filter(t => !!t),
-            distinctUntilChanged((t1: Task, t2: Task) => {
-              console.log({
-                t1: StatusPipe.getStatus(t1),
-                t2: StatusPipe.getStatus(t2)
-              });
-              return (StatusPipe.getStatus(t1) === StatusPipe.getStatus(t2)) && (t1.data.length === t2.data.length)
-            })
+            distinctUntilChanged((t1: Task, t2: Task) => (StatusPipe.getStatus(t1) === StatusPipe.getStatus(t2)) && (t1.data.length === t2.data.length))
           )
           .subscribe(task => {
             const status = StatusPipe.getStatus(task);
@@ -98,6 +94,7 @@ export class TaskComponent implements OnInit, OnDestroy {
   }
 
   onTabChange($event: MatTabChangeEvent) {
+    this.currentTab = this.tabs[$event.index].id;
     if (isA<ChunkType>(ChunkTypes, this.tabs[$event.index].type)) {
       this.ds.getTaskData(this.tabs[$event.index].id);
     }
