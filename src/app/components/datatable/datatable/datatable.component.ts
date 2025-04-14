417e2ab005ef54323d66a27b2501c7ae7704c7fe
paflov <philipp.franck@hu-berlin.de>
2025-04-14 12:12:09 +0200

    Frontend: Convenience Feature: Optionally hide unimportant columns

--- src/app/components/datatable/datatable/datatable.component.ts
+++ src/app/components/datatable/datatable/datatable.component.ts
@@ -12,6 +12,9 @@ import { DataService } from '../../../services/data.service';
 import { MatSort, MatSortHeader } from '@angular/material/sort';
 import { ResponseRow } from '../../../interfaces/api.interfaces';
 import {ResponseCodeComponent} from '../../response-code/response-code.component';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatCard} from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-datatable',
@@ -28,19 +31,39 @@ import {ResponseCodeComponent} from '../../response-code/response-code.component
     MatRowDef,
     MatSortHeader,
     MatSort,
-    ResponseCodeComponent
+    ResponseCodeComponent,
+    MatCheckbox,
+    MatCard,
+    FormsModule
   ],
   templateUrl: './datatable.component.html',
   styleUrl: './datatable.component.css'
 })
 export class DatatableComponent {
   @ViewChild(MatSort) sort!: MatSort;
-  displayedColumns: string[] = ['setId', 'id', 'subForm', 'status', 'value', 'code', 'score'];
+  displayedColumns: string[];
   readonly dataSource: MatTableDataSource<ResponseRow> = new MatTableDataSource();
+  protected readonly settings = {
+    allColumns: false
+  };
+  static columnSets = {
+    'all': ['setId', 'id', 'subForm', 'status', 'value', 'code', 'score'],
+    'important': ['setId', 'status', 'value', 'code']
+  }
+
   constructor(
     public ds: DataService
   ) {
     this.dataSource.sort = this.sort;
     this.dataSource.data = this.ds.data;
+    this.displayedColumns = DatatableComponent.columnSets.important;
+  }
+
+  updateSetting(setting: keyof typeof this.settings, checked: boolean) {
+    this.settings[setting] = checked;
+    switch (setting) {
+      case 'allColumns':
+        this.displayedColumns = DatatableComponent.columnSets[checked ? 'all' : 'important'];
+    }
   }
 }
